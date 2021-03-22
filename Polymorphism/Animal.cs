f3f0d4b6c9d2ca472bbde6f7b2da52a89b37562a
Jessica Beckford <jessicabeckf@usf.edu>
2021-03-22 18:37:56 -0400

    Add project files.

---
+++ Polymorphism/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms; // Needed for MessageBox
+
+namespace Polymorphism
+{
+    class Animal
+    {
+        //Field
+        private string _species;
+        //Contructor 
+        public Animal(string species)
+        {
+            _species = species;
+        }
+
+        //Species porperty
+        public String Species
+        {
+            get { return _species; }
+            set { _species = value; }
+        }
+
+        //MakeSound Method
+        public virtual void MakeSound()
+        {
+            MessageBox.Show("Grrrrrrrr");
+        }
+    }
+}
---
+++ Polymorphism/Cat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms; // Nedeed for MessageBox
+
+namespace Polymorphism
+{
+    class Cat : Animal
+    {
+        // Field
+        private String _name;
+
+        // Constructor
+        public Cat (string name)
+            : base("Cat")
+        {
+            _name = name;
+        }
+
+        //Name property
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        //MakeSound Method
+        public override void MakeSound()
+        {
+            MessageBox.Show("Meow");
+        }
+    }
+}
